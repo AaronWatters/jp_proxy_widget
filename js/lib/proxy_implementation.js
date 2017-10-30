427469d8f5b8f2a513abcba14119a24dc9b4eb8e
Aaron Watters <awatters@simonsfoundation.org>
2017-10-30 15:43:37 -0400

    rename views and models

--- js/lib/proxy_implementation.js
+++ js/lib/proxy_implementation.js
@@ -17,33 +17,32 @@ var _ = require('lodash');
 
 // When serialiazing the entire widget state for embedding, only values that
 // differ from the defaults will be specified.
-var HelloModel = widgets.DOMWidgetModel.extend({
+var JSProxyModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-        _model_name : 'HelloModel',
-        _view_name : 'HelloView',
+        _model_name : 'JSProxyModel',
+        _view_name : 'JSProxyView',
         _model_module : 'jp_proxy_widget',
         _view_module : 'jp_proxy_widget',
         _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0',
-        value : 'Hello World'
+        _view_module_version : '0.1.0'
     })
 });
 
 
 // Custom View. Renders the widget model.
-var HelloView = widgets.DOMWidgetView.extend({
+var JSProxyView = widgets.DOMWidgetView.extend({
     render: function() {
-        this.value_changed();
-        this.model.on('change:value', this.value_changed, this);
+        //this.value_changed();
+        //this.model.on('change:value', this.value_changed, this);
     },
 
     value_changed: function() {
-        this.el.textContent = this.model.get('value');
+        //this.el.textContent = this.model.get('value');
     }
 });
 
 
 module.exports = {
-    HelloModel : HelloModel,
-    HelloView : HelloView
+    JSProxyModel : HelloModel,
+    JSProxyView : HelloView
 };
