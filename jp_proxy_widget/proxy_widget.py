953c956163004546ad77e83d3af1971a6a69245b
Aaron Watters <awatters@simonsfoundation.org>
2017-10-31 10:27:26 -0400

    try to fix name mapping again

--- jp_proxy_widget/proxy_widget.py
+++ jp_proxy_widget/proxy_widget.py
@@ -6,8 +6,8 @@ class JSProxyWidget(widgets.DOMWidget):
     """Introspective javascript proxy widget."""
     _view_name = Unicode('JSProxyView').tag(sync=True)
     _model_name = Unicode('JSProxyModel').tag(sync=True)
-    _view_module = Unicode('jp_proxy_widget').tag(sync=True)
-    _model_module = Unicode('jp_proxy_widget').tag(sync=True)
+    _view_module = Unicode('proxy_implementation').tag(sync=True)
+    _model_module = Unicode('proxy_implementation').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
 
