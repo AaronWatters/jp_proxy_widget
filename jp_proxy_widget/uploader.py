45186aea52a8acbcaca9565fbd873f8ebed15787
Aaron Watters <awatters@simonsfoundation.org>
2018-04-24 15:16:41 -0400

    add support for reading large files in chunks

--- jp_proxy_widget/uploader.py
+++ jp_proxy_widget/uploader.py
@@ -4,6 +4,7 @@ Upload files to jupyter server location using jp_proxy.
 
 import jp_proxy_widget
 from IPython.display import display
+from traitlets import Unicode, HasTraits
 
 js_files = ["js/simple_upload_button.js"]
 
@@ -11,26 +12,31 @@ def _load_required_js(widget):
     widget.load_js_files(filenames=js_files)
 
 def from_hex_iterator(hexcontent):
+    # xxxx try to optimize...
     for i in range(0, len(hexcontent), 2):
         hexcode = hexcontent[i: i+2]
-        char = bytes([(int(hexcode, 16))])
+        char = bytes(((int(hexcode, 16)),))
         yield char
 
-class UnicodeUploader:
+class JavaScriptError(Exception):
+    "Exception sent from javascript."
 
-    status = None
+class UnicodeUploader(HasTraits):
+
+    status = Unicode("")
     uploaded_filename = None
+    segmented = None   # no segmentation -- use chunk size instead
 
     def __init__(self, html_title=None, content_callback=None, to_filename=None, size_limit=None,
-        segmented = 1000000):
+        chunk_size=1000000):
         # by default segment files into chunks to avoid message size limits
-        self.segmented = segmented
+        self.chunk_size = chunk_size
         assert content_callback is None or to_filename is None, (
             "content_callback and to_filename are mutually exclusive, please do not provide both. "
             + repr((content_callback, to_filename))
         )
         assert content_callback is not None or to_filename is not None, (
-            "content_callback or to_filename must be specified."
+            "one of content_callback or to_filename must be specified."
         )
         self.size_limit = size_limit
         self.to_filename = to_filename
@@ -43,15 +49,17 @@ class UnicodeUploader:
         level = 2
         options = self.upload_options()
         options["size_limit"] = size_limit
-
+        options["chunk_size"] = chunk_size
         proxy_callback = w.callback(self.widget_callback_handler, data="upload click", level=level,
             segmented=self.segmented)
         element = w.element()
         upload_button = element.simple_upload_button(proxy_callback, options)
         w(element.append(upload_button))
         w.flush()
+        self.status = "initialized"
 
     def show(self):
+        self.status = "displayed"
         display(self.widget)
 
     def default_content_callback(self, widget, name, content):
@@ -68,18 +76,55 @@ class UnicodeUploader:
 
     def widget_callback_handler(self, data, results):
         self.status = "upload callback called."
-        file_info = results["0"]
-        name = file_info["name"]
-        content = self.get_content(file_info)
-        content_callback = self.content_callback
-        if content_callback is None:
-            content_callback = self.default_content_callback
-        self.status = "calling " + repr(content_callback)
         try:
-            content_callback(self.widget, name, content)
+            file_info = results["0"]
+            name = file_info["name"]
+            status = file_info["status"]
+            content = self.get_content(file_info)
+            content_callback = self.content_callback
+            return self.handle_chunk(status, name, content, file_info)
         except Exception as e:
-            self.status += "\n" + repr(content_callback) + " raised " + repr(e)
+            self.status = "callback exception: " + repr(e)
             raise
+
+    chunk_collector = []
+
+    def handle_chunk(self, status, name, content, file_info):
+        "Handle one chunk of the file.  Override this method for peicewise delivery or error handling."
+        if status == "error":
+            msg = repr(file_info.get("message"))
+            exc = JavaScriptError(msg)
+            exc.file_info = file_info
+            self.status = "Javascript sent exception " + msg
+            raise exc
+        if status == "more":
+            self.chunk_collector.append(content)
+            self.progress_callback(self.chunk_collector, file_info)
+        else:
+            assert status == "done", "Unknown status " + repr(status)
+            self.chunk_collector.append(content)
+            all_content = self.combine_chunks(self.chunk_collector)
+            self.chunk_collector = []
+            content_callback = self.content_callback
+            if content_callback is None:
+                content_callback = self.default_content_callback
+            self.status = "calling " + repr(content_callback)
+            try:
+                content_callback(self.widget, name, all_content)
+            except Exception as e:
+                self.status += "\n" + repr(content_callback) + " raised " + repr(e)
+                raise
+
+    def progress_callback(self, chunks, file_info):
+        size = file_info["size"]
+        got = 0
+        for c in chunks:
+            got += len(c)
+        pct = int((got * 100)/size)
+        self.status = "received %s of %s (%s%%)" % (got, size, pct)
+
+    def combine_chunks(self, chunk_list):
+        return u"".join(chunk_list)
 
     def upload_options(self):
         "options for jquery upload plugin -- unicode, not hex"
@@ -91,7 +136,7 @@ class UnicodeUploader:
 
     def get_content(self, file_info):
         "get unicode content from file_info"
-        content = file_info["content"]
+        content = file_info.get("content")
         return content
 
 class BinaryUploader(UnicodeUploader):
@@ -103,6 +148,9 @@ class BinaryUploader(UnicodeUploader):
         return open(filename, "wb")
 
     def get_content(self, file_info):
-        hexcontent = file_info["hexcontent"]
-        content = b"".join(from_hex_iterator(hexcontent))
-        return content
+        return file_info.get("hexcontent")
+
+    def combine_chunks(self, chunk_list):
+        all_hex_content = "".join(chunk_list)
+        return b"".join(from_hex_iterator(all_hex_content))
+
