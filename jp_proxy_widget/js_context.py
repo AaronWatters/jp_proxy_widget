88fcb7e94b744c414da59b1ec28c2206d2540bc5
Aaron Watters <awatters@simonsfoundation.org>
2018-05-08 11:23:04 -0400

    refactor common usage between require_js and load_if_not_loaded

--- jp_proxy_widget/js_context.py
+++ jp_proxy_widget/js_context.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import os
 from IPython.display import display, Javascript
 import time
+import requests
 
 my_dir = os.path.dirname(__file__)
 
@@ -24,15 +25,21 @@ def get_file_path(filename, local=True):
     assert os.path.exists(result), "no such file " + repr((filename, result, user_path))
     return result
 
-def display_javascript(widget, js_filename):
-    return display(Javascript(js_filename))
+def get_text_from_file_name(filename, local=True):
+    if filename.startswith("http") and "://" in filename:
+        r = requests.get(filename)
+        return r.text
+    else:
+        path = get_file_path(filename, local)
+        return open(path).read()
 
-def eval_javascript(widget, js_filename):
-    # This will not work if javascript evaluation is disabled (in Jupyter Lab for example).
+def display_javascript(widget, js_text):
+    # This will not work if javascript is disabled.
+    return display(Javascript(data=js_text))
+
+def eval_javascript(widget, js_text):
     eval = widget.window().eval
-    # ??? are there possible encoding issues here?
-    text = open(js_filename).read()
-    widget(eval(text))
+    widget(eval(js_text))
     return widget.flush()
 
 # global default evaluation method
@@ -47,17 +54,16 @@ def load_if_not_loaded(widget, filenames, verbose=False, delay=0.1, force=False,
         evaluator = EVALUATOR  # default if not specified.
     for filename in filenames:
         loaded = False
-        js_filename = get_file_path(filename, local)
-        if force or not js_filename in LOADED_JAVASCRIPT:
+        if force or not filename in LOADED_JAVASCRIPT:
+            js_text = get_text_from_file_name(filename, local)
             if verbose:
-                print("loading javascript file", js_filename, "with", evaluator)
-            #display(Javascript(js_filename))
-            evaluator(widget, js_filename)
-            LOADED_JAVASCRIPT.add(js_filename)
+                print("loading javascript file", filename, "with", evaluator)
+            evaluator(widget, js_text)
+            LOADED_JAVASCRIPT.add(filename)
             loaded = True
         else:
             if verbose:
-                print ("not reloading javascript file", js_filename)
+                print ("not reloading javascript file", filename)
         if loaded and delay > 0:
             if verbose:
                 print ("delaying to allow JS interpreter to sync.")
