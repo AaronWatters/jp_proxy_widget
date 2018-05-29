082273e1af5951f8573fe62ff1b07a558e78bf19
Aaron Watters <awatters@simonsfoundation.org>
2018-05-29 12:04:01 -0400

    add javascript and testing files

---
+++ jp_proxy_widget/js/simple.js
@@ -0,0 +1,8 @@
+// a simple javascript file for testing loading
+
+debugger;
+
+console.log("loading simple javascript");
+
+window.the_answer = 42;
+
---
+++ jp_proxy_widget/js/simple_define.js
@@ -0,0 +1,6 @@
+// A require.js style module definition.
+
+define([], function() { 
+    console.log("loaded simple_define.js"); 
+    return {"first_3_primes": [2,3,5]};
+})
