f1f01ec24d5a44cb7643d6ef37bceb5095ce3aea
Aaron Watters <awatters@simonsfoundation.org>
2018-06-12 15:53:52 -0400

    bugfix and example download widget

--- jp_proxy_widget/downloader.py
+++ jp_proxy_widget/downloader.py
@@ -10,41 +10,37 @@ import time
 
 js_file = "js/Filesaver.js"
 
-js_helper = """
-// function(element) {
-    // debugger;
-    element.download = function(file_name, value, type) {
-        // value can be string or Uint8Array
-        var blob = new Blob([value], {type: type});
-        element.saveAs(blob, file_name);
+def load_file_saver(to_proxy_widget, when_ready):
+    if hasattr(to_proxy_widget, "saveAs_loaded"):
+        return when_ready()
+    to_proxy_widget.require_js("saveAs", js_file)
+    to_proxy_widget.js_init("""
+    debugger;
+    element.download = function(name, content, type) {
+        element.requirejs(["saveAs"], function(saveAs) {
+            var the_blob = new Blob([content], {type: type});
+            saveAs(the_blob, name);
+        });
     };
-//}
-"""
-
-def load_file_saver(to_proxy_widget, sleep=0.1):
-    w = to_proxy_widget
-    if not hasattr(to_proxy_widget, "saveAs_loaded"):
-        w.require_js("saveAs", js_file)
-        w.js_init(js_helper)
-        w.flush()
-        # sleep a little bit to allow javascript interpreter to sync
-        time.sleep(sleep)
-    w.saveAs_loaded = True
+    when_ready();
+    """, when_ready=when_ready)
+    to_proxy_widget.seg_flush()
+    to_proxy_widget.saveAs_loaded = True
 
 def saveAsUnicode(to_widget, file_name, unicode_string, type="text/plain;charset=utf-8"):
     w = to_widget
-    load_file_saver(w)
-    #element = w.element()
-    #w(element.download(file_name, unicode_string, type))
-    w.element.download(file_name, unicode_string, type)
-    w.seg_flush()
+    def when_ready():
+        w.element.download(file_name, unicode_string, type)
+        # Use a segmented flush for large data if autoflush is disabled
+        w.seg_flush()
+    load_file_saver(w, when_ready)
 
 def saveAsBinary(to_widget, file_name, byte_sequence, type="octet/stream"):
     # send the data as binary bytearray.
     data = bytearray(byte_sequence)
     w = to_widget
-    load_file_saver(w)
-    #element = w.element()
-    #w(element.download(file_name, data, type))
-    w.element.download(file_name, data, type)
-    w.seg_flush()
+    def when_ready():
+        w.element.download(file_name, data, type)
+        # Use a segmented flush for large data if autoflush is disabled
+        w.seg_flush()
+    load_file_saver(w, when_ready)
